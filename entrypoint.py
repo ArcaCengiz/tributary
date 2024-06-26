ff00016cb283c8876f58ee4d9c5a4c2dee111a9c
Arca Ege Cengiz <154221958+ArcaCengiz@users.noreply.github.com>
2024-06-26 11:50:52 +0000

    Finished task 4

--- entrypoint.py
+++ entrypoint.py
@@ -35,4 +35,22 @@ def record_engine_temperature():
 # practically identical to the above
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
-    return {"success": True}, 200
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+
+    while database.llen(DATA_KEY) > HISTORY_LENGTH:
+        database.rpop(DATA_KEY)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"engine temperature list contains these values: {engine_temperature_values}")
+
+    if len(engine_temperature_values) == 0:
+        return {"success": False}, 409  # Conflict
+
+    average = 0
+    for temp in engine_temperature_values:
+        average = average + float(temp)
+    
+    average = average / len(engine_temperature_values)
+    latest = float(engine_temperature_values[0])
+
+    logger.info(f"collect request successful")
+    return {"success": True, "latest": latest, "average": average}, 200
